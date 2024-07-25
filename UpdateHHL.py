8ce7122b94ad36bfed3f00a2ea1973c6d4e5c8ef
aRAYofsunshine07 <106277401+aRAYofsunshine07@users.noreply.github.com>
2024-07-25 11:27:18 -0400

    reorder

--- UpdateHHL.py
+++ UpdateHHL.py
@@ -4,30 +4,6 @@ from qiskit.circuit.library import QFT
 from qiskit.quantum_info import Statevector, Operator
 # Custom import and other werid imports removed 
 
-# Define matrix (A) and vector (b)
-A = np.array([[1, -1], [1, 1]])
-b = np.array([1, 0])
-
-# Normalize the vector b
-norm_b = np.linalg.norm(b)
-b_normalized = b / norm_b
-
-# Number of qubits
-n = A.shape[0]
-num_qubits = int(np.log2(n)) + 1
-
-# Create registers
-b_qubits = QuantumRegister(n, name='b')
-clock_qubits = QuantumRegister(num_qubits, name='clock')
-ancilla_qubit = QuantumRegister(1, name='ancilla')
-classical_reg = ClassicalRegister(n, name='measure')
-
-# Initialize circuit
-qc = QuantumCircuit(b_qubits, clock_qubits, ancilla_qubit, classical_reg)
-
-# Prepare initial state to vector b
-qc.initialize(b_normalized.tolist(), b_qubits)
-
 def phase_estimate(b: QuantumRegister, clock: QuantumRegister, unitary: np.ndarray) -> QuantumCircuit:
     circuit = QuantumCircuit(b, clock)
 
@@ -72,6 +48,32 @@ def inverse_qpe(qc, clock_qubits):
     qc.append(qft_inv, clock_qubits)
     qc.h(clock_qubits)
     qc.barrier()
+
+
+# Define matrix (A) and vector (b)
+A = np.array([[1, -1], [1, 1]])
+b = np.array([1, 0])
+
+# Normalize the vector b
+norm_b = np.linalg.norm(b)
+b_normalized = b / norm_b
+
+# Number of qubits
+n = A.shape[0]
+num_qubits = int(np.log2(n)) + 1
+
+# Create registers
+b_qubits = QuantumRegister(n, name='b')
+clock_qubits = QuantumRegister(num_qubits, name='clock')
+ancilla_qubit = QuantumRegister(1, name='ancilla')
+classical_reg = ClassicalRegister(n, name='measure')
+
+# Initialize circuit
+qc = QuantumCircuit(b_qubits, clock_qubits, ancilla_qubit, classical_reg)
+
+# Prepare initial state to vector b
+qc.initialize(b_normalized.tolist(), b_qubits)
+
 
 # phase Estimation
 phase_estimate(qc, b_qubits, clock_qubits, A)
