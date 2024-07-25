9ca98be8a3cefc015f9797cfae1718b9bacc697b
Alexandra Kuligowski <147344288+LexiLexi331@users.noreply.github.com>
2024-07-25 08:18:24 -0700

    Update UpdateHHL.py

--- UpdateHHL.py
+++ UpdateHHL.py
@@ -4,52 +4,6 @@ from qiskit.circuit.library import QFT
 from qiskit.quantum_info import Statevector, Operator
 # Custom import and other werid imports removed 
 
-class PhaseEstimation:
-    @staticmethod
-    def phase_estimate(b: QuantumRegister, clock: QuantumRegister, unitary: np.ndarray) -> QuantumCircuit:
-        circuit = QuantumCircuit(b, clock)
-
-        #Hadamard transform on clock qbits
-        for i in range(clock.size):
-            circuit.h(clock[i])
-        
-        # Turn U gate matrix into controlled U gate matrix where MSB is control
-        checked_unitary = np.zeros([len(unitary) * 2, len(unitary) * 2])
-        for i in range(len(unitary)):
-            for j in range(len(unitary)):
-                if i == j:
-                    checked_unitary[i][j] = 1
-                checked_unitary[i + len(unitary)][j + len(unitary)] = unitary[i][j]
-
-        # Create gate from matrix
-        unitary_gate = Operator(checked_unitary)
-
-        # Run controlled U gate the correct amount of times for each clock qubit
-        for i in range(clock.size):
-            control_array = list(range(b.size)) + [i + b.size]
-            for _ in range(2 ** i):
-                circuit.unitary(unitary_gate, control_array)
-
-        # Run QFT
-        qft = QFT(num_qubits=clock.size).to_gate()
-        circuit.append(qft, np.arange(clock.size) + b.size)
-
-        return circuit
-
-def rotate_ancilla(qc: QuantumCircuit, clock_qubits: QuantumRegister, ancilla_qubit: QuantumRegister) -> QuantumCircuit:
-    # Crotating the ancilla qubit per clock-qubit
-    for i in range(clock_qubits.size):
-        angle = 2 * np.arcsin(i / (clock_qubits.size - 1))
-        qc.cry(angle, clock_qubits[i], ancilla_qubit[0])
-
-    qc.measure(ancilla_qubit[0], classical_reg[0])
-    return qc
-
-# Updated 
-def inverse_qpe(qc: QuantumCircuit, clock_qubits: QuantumRegister) -> QuantumCircuit:
-    qc.append(QFT(len(clock_qubits), do_swaps=False).inverse(), clock_qubits)
-    return qc
-
 # Define matrix (A) and vector (b)
 A = np.array([[1, -1], [1, 1]])
 b = np.array([1, 0])
@@ -73,6 +27,50 @@ qc = QuantumCircuit(b_qubits, clock_qubits, ancilla_qubit, classical_reg)
 
 # Prepare initial state to vector b
 qc.initialize(b_normalized.tolist(), b_qubits)
+
+]def phase_estimate(b: QuantumRegister, clock: QuantumRegister, unitary: np.ndarray) -> QuantumCircuit:
+    circuit = QuantumCircuit(b, clock)
+
+    #Hadamard transform on clock qbits
+    for i in range(clock.size):
+        circuit.h(clock[i])
+        
+    # Turn U gate matrix into controlled U gate matrix where MSB is control
+    checked_unitary = np.zeros([len(unitary) * 2, len(unitary) * 2])
+    for i in range(len(unitary)):
+        for j in range(len(unitary)):
+            if i == j:
+                checked_unitary[i][j] = 1
+            checked_unitary[i + len(unitary)][j + len(unitary)] = unitary[i][j]
+
+    # Create gate from matrix
+    unitary_gate = Operator(checked_unitary)
+
+    # Run controlled U gate the correct amount of times for each clock qubit
+    for i in range(clock.size):
+        control_array = list(range(b.size)) + [i + b.size]
+        for _ in range(2 ** i):
+            circuit.unitary(unitary_gate, control_array)
+
+    # Run QFT
+    qft = QFT(num_qubits=clock.size).to_gate()
+    circuit.append(qft, np.arange(clock.size) + b.size)
+
+
+def rotate_ancilla(qc: QuantumCircuit, clock_qubits: QuantumRegister, ancilla_qubit: QuantumRegister) -> QuantumCircuit:
+    # Crotating the ancilla qubit per clock-qubit
+    for i in range(clock_qubits.size):
+        angle = 2 * np.arcsin(i / (clock_qubits.size - 1))
+        qc.cry(angle, clock_qubits[i], ancilla_qubit[0])
+
+    qc.measure(ancilla_qubit[0], classical_reg[0])
+    return qc
+
+# Updated 
+def inverse_qpe(qc: QuantumCircuit, clock_qubits: QuantumRegister) -> QuantumCircuit:
+    qc.append(QFT(len(clock_qubits), do_swaps=False).inverse(), clock_qubits)
+    return qc
+
 
 # COMPLETE CIRCUIT
 phase_estimation_circuit = PhaseEstimation.phase_estimate(b_qubits, clock_qubits, A)
