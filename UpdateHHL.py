965b280ecff7e9c0e221220082c651aea8d5e7e6
jacobalgraham <143833847+jacobalgraham@users.noreply.github.com>
2024-07-31 01:07:40 -0400

    Update UpdateHHL.py

--- UpdateHHL.py
+++ UpdateHHL.py
@@ -97,6 +97,7 @@ def main():
     # Define matrix (A) and vector (b)
     A = (1 / np.sqrt(2)) * np.array([[1, -1], [1, 1]])
     b = np.array([1, 0])
+    u = [[complex(-0.5, 0.5), complex(0.5, 0.5)], [complex(0.5, 0.5), complex(-0.5, 0.5)]]
 
     # Normalize the vector b
     norm_b = np.linalg.norm(b)
